765710d8c2eb33fb6775bf7c33a48c5677180c92
Amit Choukroun <amit@mtrn.co.il>
2019-12-28 13:50:24 +0200

    refactor: change names of server flow to fit commands name

--- routes.js
+++ routes.js
@@ -3,10 +3,10 @@ const yargs = require('yargs');
 module.exports = function(exec) {
   yargs
   .command('generate flows-server', 'generate flow server framework working with flows', () => {}, (argv) => {
-    exec('generate_flows', {});
+    exec('generate_flows_server', {});
   })
   .command('generate ws-flows-server', 'generate flow server that predefined to work with react flows', () => {}, (argv) => {
-    exec('generate_react_flows', {});
+    exec('generate_ws_flows_server', {});
   })
   .command('add <type>', 'add flows client to "angular-cli" project', () => {}, (argv) => {
     if(argv.type === 'angular-flows-client') {
