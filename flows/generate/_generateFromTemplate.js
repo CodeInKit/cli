268b37297150dfda6e4db1ab5291bbc0a101e094
Amit Choukroun <amit@mtrn.co.il>
2020-05-05 18:03:03 +0300

    chore: add scripts to package json on generating

--- flows/generate/_generateFromTemplate.js
+++ flows/generate/_generateFromTemplate.js
@@ -1,5 +1,6 @@
 
 const Promise = require('bluebird');
+const fs = require('fs').promises;
 const {spawn} = require('@codeinkit/flows-contrib-node/actions/spawn');
 const figlet = require('figlet');
 const terminalLink = require('terminal-link');
@@ -21,17 +22,32 @@ function finishMessage() {
   });
 }
 
+async function updatePackageJson(data) {
+  const package = require(`${process.cwd()}/package.json`);
+
+  package.scripts = {
+    start: 'node .',
+    deploy: 'cik deploy lambda',
+    test: 'echo "no test platform is setup yet"'
+  }
+
+  await fs.writeFile(`${process.cwd()}/package.json`, JSON.stringify(package, null, 2));
+  return data;
+}
+
 function generateFromTemplate(dirname) {
   return [
     spinnerActions.spinnerStart('Generate Flow Server!'),
     spinnerActions.spinnerMessage('npm init'),
     spawn('npm', ['init', '--yes'], {shell: true}),
-    spinnerActions.spinnerMessage('npm install @codeinkit/flows-framework'),
-    spawn('npm', ['install', '@codeinkit/flows-framework'], {shell: true}), 
+    spinnerActions.spinnerMessage('npm install @codeinkit/flows-framework @codeinkit/cli'),
+    spawn('npm', ['install', '@codeinkit/flows-framework', '@codeinkit/cli'], {shell: true}), 
     spinnerActions.spinnerMessage('git init'),
     spawn('git', ['init'], {shell: true}), 
     spinnerActions.spinnerMessage('copy template files'),
     fileActions.copyTemplates(dirname),
+    spinnerActions.spinnerMessage('update package.json'),
+    updatePackageJson,
     spinnerActions.spinnerMessage('done'),
     spinnerActions.spinnerStop,
     finishMessage
